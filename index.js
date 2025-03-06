dfcecb6565f981e4b16f6aa2c6106db91ae6fcf8
Dev-Olena <alenagruda@gmail.com>
2025-03-06 22:33:37 +0200

    add button See all, displayAllCharacters

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const BASE_URL = 'https://rickandmortyapi.com/api/character';
 //отримуємо доступ до елементів
 const charactersList = document.querySelector('#list');
 const loader = document.querySelector('#loader');
-
+const btnAll = document.querySelector('#btn-all');
+const btnBlock = document.querySelector('.button-block');
 
 
 
 //створюємо екземпляр APIHandler з базовою url
 const api = new APIHandler(BASE_URL);
 const quantityForHomePage = 4;
+
+
+//додаємо обробник події на клік по кнопці See all
+btnAll.addEventListener('click', displayAllCharacters);
 
 
 // відображення рандомних 4 персонажей при завантаженні застосунку
@@ -28,6 +33,20 @@ async function displayCharacters() {
     charactersList.append(...cards);
 }
 
+async function displayAllCharacters() {
+    charactersList.innerHTML= '';
+    showLoader();
+    const charactersArray = await api.getAllCharacters();
+    const cards = charactersArray.map(characterData => {
+        const character = new Character(characterData);
+        return character.render();
+    });
+    removeLoader();
+    charactersList.append(...cards);
+    btnBlock.innerHTML = ""
+}
+
+
 
 //функція для відображення лоадера
 const showLoader = () => {
@@ -38,6 +57,7 @@ const showLoader = () => {
 const removeLoader = () => {
     loader.classList.remove('visible');
 }
+
 
 
 
--- js/Character.js
+++ js/Character.js
@@ -7,13 +7,16 @@ class Character {
     }
 
     render() {
-        console.log(this.name);
+        // Створюємо картку персонажа
         const characterCard = document.createElement('article');
+        //Створюємо елементи картки та стилізуємо їх
         const name = document.createElement('h3');
         name.classList.add('character-name')
         name.textContent = this.name;
         const img = document.createElement('img');
         img.src = this.image;
+        //Додаємо стилі та елементи до картки
+        characterCard.classList.add('card');
         characterCard.append(img, name);
         return characterCard;
     }
